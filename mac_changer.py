e53725ee517bb9a22c9eece435634f38c2aaff36
MarkSeliter <mark.seliternikov@gmail.com>
2021-06-15 16:23:01 +0300

    started working on commandline args

--- mac_changer.py
+++ mac_changer.py
@@ -1,56 +1,47 @@
 #!/usr/bin/env python
 
+# this python2 program uses the program 'ip' and not 'ifconfig'.
+
+
 import subprocess
-
-def main(interface, mac):
-
-	# keep track if everything went smoothly
-	fail = False
+import optparse
 
 
-	print('[+] turning {interface} down...')
+def main():
+
+	parser = optparse.OptionParser()
+
+	print('[+] turning ' + inter + ' down...')
 
 	# system call to turn the interface down
-	try:
-		subprocess.call('ip link set dev {interface} down', shell=True)
+	subprocess.call(['ip', 'link', 'set', 'dev', inter, 'down'])
 	
-	# on fail
-	except:
-		fail = True
-		print('[!] please enter a valid interface')
-
-	
-	print('[+] setting MAC address of {mac} on the interface {interface}')
+	print('[+] setting MAC address of ' + mac_addr + ' on the interface ' + inter)
 
 	# for now hardcoded the MAC which it will change to
-	try:
-		subprocess.call('ip link set dev eth0 address {mac}', shell=True)
-
-	# on fail
-	except:
-		fail = True
-		print('[!] please enter a valid MAC address')
+	subprocess.call(['ip', 'link', 'set', 'dev', inter, 'address', mac_addr])
 
 
-	print('[+] turning eth0 back up')
+	print('[+] turning ' + inter + ' back up')
 
 	# system call to turn the interface up
-	try:
-		subprocess.call('ip link set dev eth0 up', shell=True)
+	subprocess.call(['ip', 'link', 'set', 'dev', inter, 'up'])
 
-	# on fail
-	except:
-		fail = True
-		print('[!] something went wrong, could not turn {interface} back up')
+	print('[+] Done')
+	print('-' * 50)
+
+	# display the result
+	subprocess.call(['ip', 'link', 'show', inter])
 
 
-	# feedback if program ran successfully
-	if fail:
-		print('[!] stopped')
-	else:
-		print('[+] done')
+def help():
+
+	print('test')
+	print('test')
+	print('test')
+	print('test')
 
 
 if __name__== '__main__':
-	main(interface, main)
 
+	main()
