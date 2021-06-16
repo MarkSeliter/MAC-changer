c7ad894af1f57438659728a170d8640faae33454
MarkSeliter <mark.seliternikov@gmail.com>
2021-06-16 18:11:09 +0300

    added the "random" option

--- mac_changer.py
+++ mac_changer.py
@@ -14,50 +14,37 @@ def main():
     # options for the parser
     parser.add_option('-i', '--interface', dest='interface', help="interface to change it's MAC address")
     parser.add_option('-m', '--mac-addr', dest='mac_address', help="specify which MAC address to use")
-    parser.add_option('-r', '--random', action='store_true' , dest='random_mac', default=True, help="generate a random MAC address (default)")
+    parser.add_option('-r', '--random', action='store_true' , dest='random_mac', help="generate a random MAC address")
 
     # output the options and arguments into options and args
     (options, args) = parser.parse_args()
 
     # check if we got an interface option
     if not options.interface:
-        print('please specify an interface to change it\'s MAC address')
+        print('please specify an interface (-i) to change it\'s MAC address')
         quit()
 
     # if we did then it will pass it into a var
     interface = options.interface
 
-    # check if we got random as an arg
-    if options.random_mac:
-        # define a var to store the random MAC address
-        random_mac = str()
-
-        # generate 6 random 1 byte sized hex numbers
-        for i in range(0, 6):
-            # rgenerate random byte sized number
-            num = random.randrange(0, 255, 1)
-
-            # if the number has only 1 digit
-            if num < 10:
-                num = int('0' + str(num))
-
-            random_mac = random_mac + str(hex(num)[2:]) + ':'
-
-        # return the result without the last column
-        mac_address = random_mac[:-1]
-
     # if not random then the specified MAC
-    elif options.mac_address:
+    if options.mac_address:
         mac_address = options.mac_address
+
+    # check if we got random as an arg
+    elif options.random_mac:
+        mac_address = generate_random_mac()
 
     # if we didn't get either, ask user to specify either MAC or RANDOM and quit
     else:
-        print('please specify a MAC address or use the --random (-r) option')
+        print('please specify a MAC address (-m / --mac-addr) or use the (-r / --random) option')
         quit()
 
     change_mac(interface, mac_address)
+    return 0
 
 
+# changes the MAC address with system commands
 def change_mac(inter, mac):
     print('[+] turning ' + inter + ' down...')
 
@@ -79,6 +66,52 @@ def change_mac(inter, mac):
 
     # display the result
     subprocess.call(['ip', 'link', 'show', inter])
+
+
+# generates random MAC address
+def generate_random_mac():
+
+    # define a string to store the result
+    random_mac = str()
+
+    # generate first unicast byte in the MAC address
+    num = random.randrange(0, 255, 1)
+
+    # checks if the last bit is multicast
+    if num & 1 == 1:
+        num -= 1
+
+    # check if the num has only 1 digit in hex
+    num = check_digit(num)
+
+    # adds the first digit to the MAC
+    random_mac = random_mac + num+ ':'
+
+    # generate 5 additional random 1 byte sized hex numbers
+    for i in range(0, 5):
+        # rgenerate random byte sized number
+        num = random.randrange(0, 255, 1)
+
+        # if the number has only 1 digit in hex
+        num = check_digit(num)
+
+        random_mac = random_mac + num+ ':'
+
+    # return the result without the last column
+    result = random_mac[:-1]
+
+    return result
+
+
+# checks if a num has 1 digits
+def check_digit(x):
+    if x < 0x10:
+        x = '0' + str(hex(x))[2:]
+    
+    else:
+        x = str(hex(x))[2:]
+
+    return x
 
 
 if __name__ == '__main__':
